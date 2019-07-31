f5cdb1cc169e34e88b5d04ddceba3fa04a4b6cfa
Paul Cortes <paulkerg@gmail.com>
2019-07-31 06:30:50 -0500

    Commit base

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// import 'package:componentes/src/pages/home_temp.dart';
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/routes/routes.dart';
+import 'package:flutter/material.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Componentes App',
+      // home:HomePage(),
+      initialRoute: '/',
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (RouteSettings settings){
+        print("Ruta llamada: ${settings.name} ");
+        return MaterialPageRoute(
+          builder: (BuildContext context) => AlertPage()
+        );
+      },
+
+      
+    );
+  }
+}
