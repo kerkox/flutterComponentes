e8e12ec6bc95d7167fb0f2786b4f884206861951
Paul Cortes <paulkerg@gmail.com>
2019-08-01 00:21:17 -0500

    Ajustes para el video No 72

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
 
 class CardPage extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Card'),
-       ),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[_cardTipo1()],
+      ),
+    );
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(
+              Icons.photo_album,
+              color: Colors.blue,
+            ),
+            title: Text("Soy el titulo de esta tarjeta"),
+            subtitle: Text("Aqui tenemos una idea de lo que pienso hacer"),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text("cancelar"),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text("Ok"),
+                onPressed: () {},
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
